d824baf990fec24de8fe685d59221d2c3ef2aa98
Dan Boxler <danbox19@msn.com>
2017-08-15 15:46:36 -0600

    adding checks to make sure we don't cause an IndexOutOfBoundsException with an empty list
    (cherry picked from commit 79a4695ef4f5a6d781bff176f076b1a4dbfe158c)

--- MPChartLib/src/main/java/com/github/mikephil/charting/renderer/LineChartRenderer.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/renderer/LineChartRenderer.java
@@ -571,7 +571,7 @@ public class LineChartRenderer extends LineRadarRenderer {
                         drawValue(c, formatter.getPointLabel(entry), x, y - valOffset, dataSet.getValueTextColor(j / 2));
                     }
 
-                    if (entry.getIcon() != null && dataSet.isDrawIconsEnabled()) {
+                    if (entry != null && entry.getIcon() != null && dataSet.isDrawIconsEnabled()) {
 
                         Drawable icon = entry.getIcon();
 
